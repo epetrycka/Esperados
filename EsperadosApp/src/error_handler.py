b1dbecba6a00edd2adc7d6caec1fa98d5c9f85f0
Eliza <elizapetrycka@gmail.com>
2025-06-09 00:35:13 +0200

    OK

--- EsperadosApp/src/error_handler.py
+++ EsperadosApp/src/error_handler.py
@@ -5,19 +5,19 @@ from antlr4.error.ErrorListener import ErrorListener
 class ErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         if str(msg).count('no viable alternative at input'):
-            if match := re.search(r"['].*[']", msg):
-                error_msg = f"\033[91mERRORE: linea {line}:{column} - syntax error at {match.group()}\033[0m"
+            if match:=re.search(r"['].*[']", msg):
+                error_msg = f"ERRORE: linea {line}:{column} - syntax error at {match.group()}"
             else:
-                error_msg = f"\033[91mERRORE: linea {line}:{column} - {msg}\033[0m"
+                error_msg = f"ERRORE: linea {line}:{column} - {msg}"
         elif re.search(r"extraneous input.*Saluton.*", msg):
-            error_msg = f"\033[91mERRORE: linea {line}:{column} - program must begin with Saluton\033[0m"
+            error_msg = f"ERRORE: linea {line}:{column} - program must begin with Saluton"
         elif re.search(r"extraneous input.*Adiau.*", msg):
-            error_msg = f"\033[91mERRORE: linea {line}:{column} - program must end with Adiau\033[0m"
-        elif re.search(r"token recognition error at: '\".*\\n'", msg):
-            error_msg = f"\033[91mERRORE: linea {line}:{column} - missing (\")\033[0m"
+            error_msg = f"ERRORE: linea {line}:{column} - program must end with Adiau"
+            
+        elif re.search(r"token recognition error at: \'\".*\\n\'", msg):
+            error_msg = f"ERRORE: linea {line}:{column} - missing (\")"
         elif re.search(r"mismatched input '.*' expecting \{.*\}", msg):
-            error_msg = f"\033[91mERRORE: linea {line}:{column} - incomplete or incorrect sentence\033[0m"
+            error_msg = f"ERRORE: linea {line}:{column} - incomplete or incorrect sentence"
         else:
-            error_msg = f"\033[91mERRORE: linea {line}:{column} - {msg}\033[0m"
-
+            error_msg = f"ERRORE: linea {line}:{column} - {msg}"
         raise Exception(error_msg)
